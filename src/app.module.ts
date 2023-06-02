4dccf28937ef2c9d53e4065e240832eddfa12e53
Gilberto Asuaje <gilberto.asuaje@epayco.com>
2023-06-02 15:35:25 -0500

    refactor: add files missed

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,27 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { EntitiesModule } from './entities/entities.module';
-
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './auth/jwt.strategy';
+import { RolesGuard } from './auth/roles.guard';
+import { AuthService } from "./auth/auth.service";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(''),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     ProductsModule,
-    EntitiesModule
+    EntitiesModule,
+    UsersModule,
+    AuthModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'secreto', 
+      signOptions: { expiresIn: '1h' },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
